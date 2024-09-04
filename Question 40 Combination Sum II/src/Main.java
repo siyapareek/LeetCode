9fb298324837f06df5499e147c5c4fff9c5b29d5
siyapareek <93838945+siyapareek@users.noreply.github.com>
2024-09-04 20:57:19 +0530

    Add files via upload

---
+++ Question 40 Combination Sum II/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+    public static List<List<Integer>> combinationSum2(int[] cand, int target) {
+        Arrays.sort(cand);
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        List<Integer> path = new ArrayList<>();
+        dfs(cand, 0, target, path, res);
+        return res;
+    }
+    public static void dfs(int[] cand, int cur, int target, List<Integer> path, List<List<Integer>> res) {
+        if (target == 0) {
+            res.add(new ArrayList(path));
+            return ;
+        }
+        if (target < 0) return;
+        for (int i = cur; i < cand.length; i++){
+            if (i > cur && cand[i] == cand[i-1]) continue;
+            path.add(path.size(), cand[i]);
+            dfs(cand, i+1, target - cand[i], path, res);
+            path.remove(path.size()-1);
+        }
+    }
+    public static void main(String[] args) {
+        int[] candidates = {10,1,2,7,6,1,5};
+        int target = 8;
+        System.out.println(combinationSum2(candidates,target));
+    }
+}
