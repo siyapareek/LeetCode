233a4ca4ebd9e70f3ecb094055aee0bb4b3d16f1
siyapareek <93838945+siyapareek@users.noreply.github.com>
2024-09-09 06:08:56 +0530

    Add files via upload

---
+++ Question 2326 Spiral Matrix IV/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int val) { this.val = val; }
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
