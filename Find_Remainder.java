298dfe8da18c93d4d9185de18cb835c88bfe652b
siyapareek <93838945+siyapareek@users.noreply.github.com>
2024-09-15 20:51:09 +0530

    Add files via upload

---
+++ Find_Remainder.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Find_Remainder {
+    public static void main(String args[]){
+        int test=0,divi=0,numb=0;
+        int i=0,rema=0;
+        Scanner sc=new Scanner(System.in);
+        test=sc.nextInt();
+        for(i=0;i<test;i++){
+            numb=sc.nextInt();
+            divi=sc.nextInt();
+            rema=numb%divi;
+            System.out.println(rema);
+        }
+    }
+}
