e66e573c5addd0f5056e97ee7a87384b075023a1
siyapareek <93838945+siyapareek@users.noreply.github.com>
2024-10-09 17:09:25 +0530

    Add files via upload

---
+++ Miami_GP.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Miami_GP {
+    public static void main (String[] args) throws java.lang.Exception {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while(T-->0) {
+            int buyingValue = sc.nextInt();
+            int sellingValue = sc.nextInt();
+            if(sellingValue>buyingValue) System.out.println("PROFIT");
+            else if(sellingValue<buyingValue) System.out.println("LOSS");
+            else System.out.println("NEUTRAL");
+        }
+    }
+}
