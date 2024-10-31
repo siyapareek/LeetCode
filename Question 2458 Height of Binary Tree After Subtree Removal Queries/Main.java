7acc619f5f67dd00aa256724a071dd78d212b7b2
siyapareek <93838945+siyapareek@users.noreply.github.com>
2024-10-31 10:30:34 +0530

    Add files via upload

---
+++ Question 2458 Height of Binary Tree After Subtree Removal Queries/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int val) { this.val = val; }
+    }
+    int depth[];
+    int levelArr[];
+    int max1[];
+    int max2[];
+    public int height(TreeNode root, int level){
+        if(root==null) return 0; 
+        levelArr[root.val] = level; 
+        depth[root.val] = 1 + Math.max(height(root.left, level+1), height(root.right, level+1));
+        if(max1[level]<depth[root.val]) {
+            max2[level] = max1[level];
+            max1[level] = depth[root.val];
+        }
+        else if(max2[level]<depth[root.val]) max2[level] = depth[root.val]; 
+        return depth[root.val];
+    }
+    public int[] treeQueries(TreeNode root, int[] queries) {
+        depth = new int[100001];
+        levelArr = new int[100001];
+        max1 = new int[100001];
+        max2 = new int[100001];
+        height(root, 0);
+        for(int i=0; i<queries.length; i++){
+            int q = queries[i];
+            int level = levelArr[q];
+            queries[i] = (max1[level]==depth[q]? max2[level] : max1[level]) + level - 1;     
+        }
+       return queries; 
+    }
+}
