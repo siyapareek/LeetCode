add965e3a45cfc94b9a14e0ad9786a0e189cbf43
siyapareek <93838945+siyapareek@users.noreply.github.com>
2024-11-15 11:05:17 +0530

    Add files via upload

---
+++ Question 1233 Remove Sub-Folders from the Filesystem/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+    public List<String> removeSubfolders(String[] folder) {
+        Arrays.sort(folder);
+        List<String> ans = new ArrayList<>();
+        ans.add(folder[0]);
+        for (int i = 1; i < folder.length; i++) {
+            String lastFolder = ans.get(ans.size() - 1) + "/";
+            if (!folder[i].startsWith(lastFolder)) ans.add(folder[i]);
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
